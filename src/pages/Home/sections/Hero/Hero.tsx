698dc5820128b4bab871c33110ad2aa61016a186
davi <davibritojunior1@gmail.com>
2024-07-10 18:15:38 -0300

    :test_tube: Teste de aplicação

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import { styled } from "@mui/material"
+
+
+function Hero() {
+
+    const StyledHero = styled("div")({
+        backgroundColor: "black"
+    })
+
+
+    return (
+     <>
+       <StyledHero>
+
+        Hero 
+       </StyledHero>
+     </>
+   )
+ }
+ 
+ export default Hero
+ 
