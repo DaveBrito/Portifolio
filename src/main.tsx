0d4ab45beec0c508f0058b30936d5d75148f2225
davi <davibritojunior1@gmail.com>
2024-07-11 13:41:46 -0300

    :feat: Configuração dos materiais
    - Adicionado responsividade
    - Importado CSS do material ui
    - Configuração do tema e fonte da escrita

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import CssBaseline from '@mui/material/CssBaseline/CssBaseline'
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
+import theme from './theme.ts'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+      
+    </ThemeProvider>
   </React.StrictMode>,
 )
